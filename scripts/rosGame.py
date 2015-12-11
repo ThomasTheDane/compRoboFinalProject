975a959df788a8271c4d8987f37a5a16dd1ec0d8
aclairekeum <ayoung.keum@students.olin.edu>
2015-12-11 15:13:53 -0500

    red circles working!

--- scripts/rosGame.py
+++ scripts/rosGame.py
@@ -46,6 +46,9 @@ class Neatobot:
         self.cz = 0
         self.theta = 0
         self.swap_new_coord = None
+        self.coins = []
+        self.coinPixels = []
+        self.coinVisible = True
         # rospy.Subscriber("STAR_pose_euler_angle", Vector3, self.processEulerAngle)  
         rospy.Subscriber("/camera/image_raw", Image, self.process_image)
         rospy.Subscriber("STAR_pose_continuous",PoseStamped, self.processLocation)
@@ -71,32 +74,43 @@ class Neatobot:
         # TODO
         # theta should be negated
 
-    def transformWorldToCamera(self, coord):
-        rotat = rotation_matrix(self.theta,[0,0,1])
-        trans = np.matrix([[1,0,0,-self.cx],[0,1,0,-self.cy],[0,0,1,-self.cz],[0,0,0,1]])
+    def transformWorldToCamera(self, aCoin):
+        self.coinPixels = []
+        aCoinPixels = []
 
-        new_coord = np.dot(rotat, np.dot(trans, coord))
-        # self.pixelate(new_coord[:3])
-        self.pixelate(new_coord[:,:-1])
+        for aCoord in aCoin:
+
+            rotat = rotation_matrix(self.theta,[0,0,1])
+            trans = np.matrix([[1,0,0,-self.cx],[0,1,0,-self.cy],[0,0,1,-self.cz],[0,0,0,1]])
+
+            new_coord = np.dot(rotat, np.dot(trans, aCoord))
+            
+
+            aCoinPixels.append(self.pixelate(new_coord[:3]))
+
+        self.coinPixels.append(aCoinPixels)
+        
 
     def pixelate(self, new_coord):
 
-    	for i in range(new_coord.shape[0]):
-    		new_coord[i][0]=-new_coord[i][1]
-    		new_coord[i][1]=-new_coord[i][2]
-    		new_coord[i][2]=new_coord[i][0]
+    	# for i in range(new_coord.shape[0]): 
+    	# 	b[i][0]=-new_coord[i][1]
+    	# 	b[i][1]=-new_coord[i][2]
+    	# 	b[i][2]= new_coord[i][0]
     		
-        # new_coord = [-new_coord[1,0], -new_coord[2,0], new_coord[0,0]]
-        self.swap_new_coord = new_coord
+        new_coord = [-new_coord[1,0], -new_coord[2,0], new_coord[0,0]]
+        if new_coord[2] < 0:
+        	self.coinVisible = False
 
         new_coord = np.array([new_coord])
         # points = cv2.projectPoints(new_coord, (0,0,0), (0,0,0), self.K, self.D)
         # self.points = points[0]
         
         p = self.K.dot(new_coord.T)
-        self.pixel =  p / p[2]
+        return p / p[2]
         # print "self.pixel", self.pixel
-        
+    
+    
 
     def get_camerainfo(self,msg):
         self.D = msg.D
@@ -105,27 +119,26 @@ class Neatobot:
     def process_image(self,msg):
 
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        if self.pixel !=None :
-            print "self swap coord", self.swap_new_coord
 
-            if self.swap_new_coord[2] > 0:
-	            cv2.circle(self.cv_image,(int(self.pixel[0]), int(self.pixel[1])), 120, (0, 0, 255))
+        if self.coinPixels:
+            for aCoin in self.coinPixels:
+                for aPixel in aCoin:
+
+                    if self.coinVisible:
+                        cv2.circle(self.cv_image,(int(aPixel[0]), int(aPixel[1])), 20, (0, 0, 255))
 
         cv2.imshow('video_window', self.cv_image)
         cv2.waitKey(10)
-
-    def 
 
     def run(self):
         """ The main run loop, in this node it doesn't do anything """
         r = rospy.Rate(5)
         while not rospy.is_shutdown():
             if self.theta!=0:
-                coin_coord = np.array([[[1], [0], [0], [1]],[[1.5], [0], [0], [1]],[[1.5], [0.5], [0], [1]],[[1], [0.5], [0], [1]]],dtype='float32')
+                coin_coord = np.array([[[1], [0], [0], [1]],[[1], [0], [.2], [1]],[[1], [.2], [0], [1]],[[1], [.2], [.2], [1]]],dtype='float32')
                 self.transformWorldToCamera(coin_coord)
             r.sleep()
 
 if __name__ == '__main__':
     nb = Neatobot()
     nb.run()
-k
